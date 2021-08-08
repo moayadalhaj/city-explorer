5218e63ee65491c9180f3547f5776a76da206c7a
moayadalhaj <moayad.alhaj21@gmail.com>
2021-08-08 19:53:28 +0300

    solve lab06

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { Card, ListGroup } from 'react-bootstrap';
+import { Card, Alert } from 'react-bootstrap';
 
 class Location extends Component {
   render() {
-    return (this.props.locationName && <div>
-      <Card className="m-5" style={{ maxWidth: '350px' }}>
-        <Card.Header className="mx-auto">Location Details</Card.Header>
-        <ListGroup variant="flush">
-          <ListGroup.Item>Location Name: {this.props.locationName}</ListGroup.Item>
-          <ListGroup.Item>Latitude: {this.props.latitude}</ListGroup.Item>
-          <ListGroup.Item>Longitude: {this.props.longitude}</ListGroup.Item>
-        </ListGroup>
-      </Card>
-    </div >
+    return (<>
+      {
+        this.props.error && (<Alert variant="danger" className="ms-5" style={{ width: '300px' }}>
+          <Alert.Heading>Error</Alert.Heading>
+          <p>Unable to geocode</p>
+        </Alert>)
+      }
+
+      {this.props.locationName && (<Card className="mx-auto" style={{ width: '700px', height: '400px' }}>
+        <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_TOKEN}&center=${this.props.latitude},${this.props.longitude}&zoom=16&size=700x400&markers=icon:large-red-cutout|${this.props.latitude},${this.props.longitude}`} alt="map" />
+        <Card.Body className="bg-dark">
+          <Card.Title className="text-white mb-2">{this.props.locationName}</Card.Title>
+          <Card.Text className="bg-primary text-white p-2 rounded" style={{ width: "200px" }}>Latitude: {this.props.latitude}</Card.Text>
+          <Card.Text className="bg-primary text-white p-2 rounded" style={{ width: "200px" }}>Longitude: {this.props.longitude}</Card.Text>
+        </Card.Body>
+      </Card>)}
+    </>
     )
   }
 }
